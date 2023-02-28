9be49ff3118f78a2ea8fcd2a3acb40815e59cd18
Raphael Braga <raffael.info@gmail.com>
2023-02-28 15:24:18 -0300

    chore: oil change form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { HomeClientComponent } from './home-client/home-client.component';
 import { HomeClientMComponent } from './home-client-m/home-client-m.component';
 import { AdminComponent } from './admin/admin.component';
 import { ClientCarListComponent } from './client-car-list/client-car-list.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeClientComponent, HomeClientMComponent, AdminComponent, ClientCarListComponent],
+  declarations: [AppComponent, HeaderComponent, HomeClientComponent, HomeClientMComponent, AdminComponent, ClientCarListComponent, ServiceListComponent],
   imports: [BrowserModule, AppRoutingModule, PrimengModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/common/primeng/primeng.module.ts
+++ src/app/common/primeng/primeng.module.ts
@@ -3,9 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { BadgeModule } from 'primeng/badge';
 import { StyleClassModule } from 'primeng/styleclass';
 import { CardModule } from 'primeng/card';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+import { MenuModule } from 'primeng/menu';
+import { DialogModule as ModalModule } from 'primeng/dialog';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { SplitterModule } from 'primeng/splitter';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DividerModule } from 'primeng/divider';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   declarations: [],
@@ -15,16 +26,38 @@ import { CardModule } from 'primeng/card';
     RippleModule,
     BadgeModule,
     InputTextModule,
+    InputTextareaModule,
     StyleClassModule,
     CardModule,
+    FormsModule,
+    RatingModule,
+    MenuModule,
+    ModalModule,
+    CheckboxModule,
+    DropdownModule,
+    SplitterModule,
+    InputNumberModule,
+    DividerModule,
+    CalendarModule,
   ],
   exports: [
     ButtonModule,
     RippleModule,
     BadgeModule,
     InputTextModule,
+    InputTextareaModule,
     StyleClassModule,
     CardModule,
+    FormsModule,
+    RatingModule,
+    MenuModule,
+    ModalModule,
+    CheckboxModule,
+    DropdownModule,
+    SplitterModule,
+    InputNumberModule,
+    DividerModule,
+    CalendarModule,
   ],
 })
 export class PrimengModule {}
