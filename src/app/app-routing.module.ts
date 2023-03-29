1d723d62ef4bab6c6232d8ec81dd148af0c324ba
Phillipe Menezes <phillipe.o.m@hotmail.com>
2023-03-29 19:04:06 -0300

    refactor: added routes and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: AdminComponent },
+  { path: 'events', component: AdminComponent},
+  { path: 'transfer', component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
